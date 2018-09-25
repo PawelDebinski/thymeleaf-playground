2fe994697b2922697477d37a27cfd55746224c65
Pawel Debinski <spokojasne@interia.eu>
2018-09-25 09:30:20 +0200

    form - added dropdown menu, closes #7

--- src/main/java/pl/pawel/controller/UserFormController.java
+++ src/main/java/pl/pawel/controller/UserFormController.java
@@ -3,14 +3,18 @@ package pl.pawel.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+import pl.pawel.CountriesList;
+
+import java.util.Map;
+import java.util.TreeMap;
 
 @Controller
 public class UserFormController {
 
     @GetMapping("/new-user")
     public ModelAndView showNewUserForm() {
+        Map<String, String> map = CountriesList.getCountries();
 
-
-        return new ModelAndView("new-user");
+        return new ModelAndView("new-user", "countries", map);
     }
 }
