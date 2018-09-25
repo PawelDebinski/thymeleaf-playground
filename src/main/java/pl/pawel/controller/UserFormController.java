8fe4135c7b9f971460784675fa05113f1f521cfc
Pawel Debinski <spokojasne@interia.eu>
2018-09-25 10:46:21 +0200

    form - added form data to controller, closes #10

--- src/main/java/pl/pawel/controller/UserFormController.java
+++ src/main/java/pl/pawel/controller/UserFormController.java
@@ -2,13 +2,14 @@ package pl.pawel.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.PostMapping;
 import pl.pawel.AddUserModel;
 import pl.pawel.CountriesList;
 
+import javax.validation.Valid;
 import java.util.Map;
-import java.util.TreeMap;
 
 @Controller
 public class UserFormController {
@@ -20,5 +21,13 @@ public class UserFormController {
         model.addAttribute("addUserModel", new AddUserModel());
 
         return ("new-user");
+    }
+
+    @PostMapping("/saveUser")
+    public String saveUser(@Valid AddUserModel addUserModel, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            return "new-user";
+        }
+        return "user-added";
     }
 }
